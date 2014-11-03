b80a4a9b6439586bcebad7371ab15b01ca742305
Paula Denner <phrfsb@hautacam.epp.warwick.ac.uk>
2014-11-03 15:13:20 +0000

    this is a change

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  //this is a comment
+
+  cout << "hello World" << endl;
+  return 0;
+
+}
---
+++ ex2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+  //programme that asks for two doubles as input from the user and then prints out the product on the screen
+
+  double a;
+  double b;
+
+  cout << "please enter double a and double b to multiply them" <<endl;
+
+  cin >> a >> b;
+
+  cout << "the product of a and b is: " << a*b <<endl;
+
+  return 0;
+
+}
