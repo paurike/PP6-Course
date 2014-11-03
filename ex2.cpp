45bcd2bbdad4f0e610c10c2f59eff2f7853d9730
Paula Denner <phrfsb@hautacam.epp.warwick.ac.uk>
2014-11-03 16:40:47 +0000

    Make a calculator with 4 basic operations, user input and tests

--- ex2.cpp
+++ ex2.cpp
@@ -1,19 +1,72 @@
 #include<iostream>
+#include<climits>
 
 using namespace std;
 
 int main() {
+
+
+
+  while(
 
   //programme that asks for two doubles and lets the user decide which operation to perform on them
   
   double a;
   double b;
 
-  cout << "please enter double a and double b to multiply them" <<endl;
+  int c;
+
+  cout << "please enter 1 for addition, 2 for multiplication, 3 for subtraction or 4 for division" <<endl;
+
+  cin >> c;
+  
+  int t = 0;
+
+
+  while(0 == t) {
+
+    if((c != 1) && (c != 2) && (c != 3) && (c != 4) )
+    {cout << "Please chose between 1, 2, 3 and 4 to chose an operation!!"<< endl;
+     cin.clear();
+     cin.ignore(INT_MAX, '\n');
+     cin >> c;
+    }
+  else{t = 1;}
+
+  }
+  
+
+  cout << "please enter two numbers that you want to perform the operation on!";
 
   cin >> a >> b;
 
-  cout << "the product of a and b is: " << a*b <<endl;
+ 
+  
+
+  if(c==1){
+    cout << "The sum of " << a << " and " << b << " is: " << a+b << endl;
+    return 0;
+  }
+
+  if(c==2){
+    cout << "The product of " << a << " and " << b << " is: " << a*b << endl;
+    return 0;
+  }
+
+  if(c==3){
+    cout << "The difference of " << a << " and " << b << " is: " << a-b << endl;
+    return 0;
+  }
+
+
+
+  if(c==4){
+    if (0==b)
+    {cout << "Don't devide by zero, it implodes the universe!!" << endl;
+    }
+    else{cout << "The fraction of " << a << " and " << b << " is: " << a/b << endl;}
+  }
+
 
   return 0;
 
