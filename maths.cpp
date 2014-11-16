b3ec5e3c574e2207e70875083a7e2b9e40afca6c
Paula Denner <P.F.Denner@warwick.ac.uk>
2014-11-16 23:33:30 +0000

    outsource the basics arithmetic functions into a maths file

---
+++ maths.cpp
@@ -0,0 +1,32 @@
+#include "maths.hpp"
+
+
+//Basic arithmetic functions for pp6calculator
+
+double add(double a, double b){
+  double res = a+b;
+  return res;
+
+}
+
+
+double subtract(double a, double b) {
+  double res= a-b;
+  return res; 
+
+
+}
+
+
+double multiply(double a, double b) {
+  double res = a*b;
+  return res; 
+
+}
+
+
+double divide(double a, double b) {
+  double res = a/b; 
+  return res;
+
+}
