5081f8758ed528aeb01de5090491154b857fbd61
Paula Denner <P.F.Denner@warwick.ac.uk>
2014-11-17 00:13:28 +0000

    implement bubble sort algorithm

---
+++ bubble_sort.hpp
@@ -0,0 +1,7 @@
+#ifndef PP6CALCULATOR_BUBBLE_SORT_HPP
+#define PP6CALCULATOR_BUBBLE_SORT_HPP
+
+void change (double& a, double& b);
+void bubble_sort(double array[], int length);
+
+#endif //PP6CALCULATOR_BUBBLE_SORT_HPP
