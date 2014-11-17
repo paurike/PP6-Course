f8c34687f55506dee6e8bad09443bfb5a69dabeb
Paula Denner <P.F.Denner@warwick.ac.uk>
2014-11-17 00:01:12 +0000

    header file for maths.cpp

---
+++ maths.hpp
@@ -0,0 +1,11 @@
+#ifndef PP6CALCULATOR_MATHS_HPP
+#define PP6CALCULATOR_MATHS_HPP
+
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+
+#endif 
