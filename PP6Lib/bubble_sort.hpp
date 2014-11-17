7d5948811a814b02d1539ea39fe16e3d3680780d
Paula Denner <P.F.Denner@warwick.ac.uk>
2014-11-17 22:54:21 +0000

    correct naming

--- PP6Lib/bubble_sort.hpp
+++ PP6Lib/bubble_sort.hpp
@@ -2,6 +2,6 @@
 #define PP6CALCULATOR_BUBBLE_SORT_HPP
 
 void change (double& a, double& b);
-void bubble_sort(double array[], int length);
+void sort(double array[], int length);
 
 #endif //PP6CALCULATOR_BUBBLE_SORT_HPP
